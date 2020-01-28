c7474571b535f5edd6be94b144999b893d98d39a
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-28 14:50:31 -0800

    react

---
+++ src/main/resources/static/precache-manifest.9fd51fc6481dfa1334a759f52318ac8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6396e474b9426b9cb7f167fbab0c1f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b374432cfdc0b37e9a53",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1db749b84e5e858f6c9c",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "b374432cfdc0b37e9a53",
+    "url": "/static/js/2.75b4d1bf.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.75b4d1bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1db749b84e5e858f6c9c",
+    "url": "/static/js/main.899bb480.chunk.js"
+  },
+  {
+    "revision": "b47d682f800ac873db0c",
+    "url": "/static/js/runtime-main.04b7d0ad.js"
+  }
+]);
